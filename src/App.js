f303e4c993d961a9041d929ec42aef8499835c06
Casey <89794911+cmurphy20-CVTC@users.noreply.github.com>
2023-04-10 20:30:37 -0500

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,31 +1,40 @@
 import React from 'react';
 import Home from './pages/Home';
+import Logo from './assets/Small Fry Thai.png';
 import Menu from './pages/Menu';
 import About from './pages/About';
 import {Link, Route, Routes} from "react-router-dom";
-import './App.css';
 import Footer from "./components/Footer";
 import Schedule from './pages/Schedule';
 
 function App() {
   return (
-    <>
+    <div className='container-fluid'>
     
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/menu">Menu</Link>
-          </li>
-          <li>
-            <Link to="/schedule">Schedule</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-        </ul>
+      <nav className='navbar navbar-expand-lg sticky-top'>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarContent">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div id="navbarContent" className='collapse navbar-collapse justify-content-start'>
+
+        <img id="brandLogo" src={Logo} alt="Small Fry Thai Logo"></img>
+
+          <ul className='nav nav-pills nav-fill '>
+            <li className='nav-item'>
+              <Link className="nav-link" to="/">Home</Link>
+            </li>
+            <li className='nav-item'>
+              <Link className="nav-link" to="/menu">Menu</Link>
+            </li>
+            
+            <li className='nav-item'>
+              <Link className="nav-link" to="/schedule">Schedule</Link>
+            </li>
+            <li className='nav-item'>
+              <Link className="nav-link" to="/about">About</Link>
+            </li>
+          </ul>
+        </div>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
@@ -36,7 +45,7 @@ function App() {
 
       <Footer />
     
-    </>
+    </div>
   );
 }
 
